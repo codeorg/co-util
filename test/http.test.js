354924b5fb42b72dccd33729f2f55d48897a6288
codeorg <codeasy@163.com>
2020-03-21 10:49:47 +0800

    v1.0.97

--- test/http.test.js
+++ test/http.test.js
@@ -5,20 +5,11 @@ import test from 'ava';
 import util from '../lib'
 
 test('util.http.post', async t => {
-    let doc = await util.http.post('http://japi.juhe.cn/rank/getRankType?key=myKey');
-    t.deepEqual( doc, {
-        "error_code": 10001,
-        "reason": "无效的key！",
-        "result": "myKey"
-    });
+    let doc = await util.http.post('https://api.weixin.qq.com/sns/oauth2/access_token');
+    t.deepEqual( doc.errcode, 41002);
 });
 test('util.http.get', async t => {
-    let doc = await util.http.get('http://apis.juhe.cn/mobile/get?phone=13429667914&key=myKey');
-    t.deepEqual( doc, {
-        "resultcode": "101",
-        "reason": "错误的请求KEY",
-        "result": null,
-        "error_code": 10001
-    });
+    let doc = await util.http.get('https://api.weixin.qq.com/sns/oauth2/access_token');
+    t.deepEqual( doc.errcode, 41002);
 
 });
